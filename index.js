e1dee704bff89264b87c9058a59858235cfbf0c2
heteng <heteng@umich.edu>
2022-07-31 16:32:02 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds
+    ]
+});
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login(process.env.DISCORD_TOKEN);
