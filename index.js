10c732e1b40c376fa5af661627c91d8e1d4a9e5e
heteng <heteng@umich.edu>
2022-07-31 17:05:31 -0400

    Implemented basic working command

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
     intents: [
-        GatewayIntentBits.Guilds
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessages
     ]
 });
 
 client.once('ready', () => {
     console.log('Ready!');
+});
+
+client.on("messageCreate", async message => {
+    if (message.content.startsWith(">")) {
+        if (message.content.substring(1).toLowerCase() === "ping") {
+            message.reply("Pong");
+        }
+    }
 });
 
 client.login(process.env.DISCORD_TOKEN);
