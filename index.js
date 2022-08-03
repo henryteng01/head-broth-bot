8de162e9abca93c86046ad9002d844a19e0748fe
heteng <heteng@umich.edu>
2022-08-02 23:01:50 -0400

    added a few more basic commands involving server and user info

--- index.js
+++ index.js
@@ -13,11 +13,17 @@ client.once('ready', () => {
     console.log('Ready!');
 });
 
-client.on("messageCreate", async message => {
-    if (message.content.startsWith(">")) {
-        if (message.content.substring(1).toLowerCase() === "ping") {
-            message.reply("Pong");
-        }
+client.on("interactionCreate", async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    } else if (commandName === 'server') {
+        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+    } else if (commandName === 'user') {
+        await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
     }
 });
 
